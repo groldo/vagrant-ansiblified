af382ee30e1e29ec0a1159b6d778e8a8f4f321fc
groldo <groldo@dumpme.de>
2021-11-23 14:20:17 +0100

    added comment for easy deploy

--- preinstall.sh
+++ preinstall.sh
@@ -6,3 +6,10 @@ ansible --version || (
     sudo -E apt-get -q --option \"Dpkg::Options::=--force-confold\" --assume-yes install -y -qq python3-pip     
     sudo pip3 install ansible 
 )
+
+# Only mind when running on hyper-v
+# Installs xrdp for enahnced session mode
+# Got to be run twice with reboots
+# wget -O install.sh https://raw.githubusercontent.com/Hinara/linux-vm-tools/ubuntu20-04/ubuntu/20.04/install.sh
+# sudo chmod +x install.sh
+# sudo ./install.sh || :
